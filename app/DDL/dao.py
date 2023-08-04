db48925329f1234f753d78d9a3e5829e11cfdba2
NicholasDRR <nicholasreis00@gmail.com>
2023-08-04 02:30:31 -0300

    Adicionando .env

--- app/DDL/dao.py
+++ app/DDL/dao.py
@@ -1,14 +1,22 @@
 import pymongo
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+HOST = os.getenv("HOST")
+DB = os.getenv("DATABASE")
+COLLECTION = os.getenv("COLLECTION")
+
 
 def connect_mongo():
     
-    client = pymongo.MongoClient('mongodb://localhost:27017')
+    client = pymongo.MongoClient(HOST)
 
-    mydb = client['AppAnimal']
+    mydb = client[DB]
 
-    collection = mydb['Animals']
+    collection = mydb[COLLECTION]
 
-    information = mydb.dataanimals
 
     return collection
 
--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from app.routes import animal
+
 from uuid import uuid4
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
