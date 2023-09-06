1d6a078193042b212cfb9fa60d9500a3ecef62f2
NicholasDRR <nicholasreis00@gmail.com>
2023-09-06 04:54:43 -0300

    feat(animal): adding a template for create, list and list_by_id

--- app/utils.py
+++ app/utils.py
@@ -2,10 +2,16 @@ import re
 from fastapi import status, HTTPException
 
 
-object_id_pattern = r"^[0-9a-fA-F]{24}$"
+dict_regex = {
+    "object_id pattern": r"^[0-9a-fA-F]{24}$",
+    "gender pattern": r"male|female",
+    "neutering pattern": r"spayed|neutered",
+    "date pattern": r"^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\d{4}$"
+    
+}
 
 
 def validate_id(id: int):
     
-    if not re.match(object_id_pattern, id):
+    if not re.match(dict_regex["object_id pattern"], id):
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail={"msg": "Invalid id!"})
